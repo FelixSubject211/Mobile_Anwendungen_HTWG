78f2fe0e513b46d45c6118f6d5e95fc66a7e5074
felixfischer <ff@mindhoox.de>
2024-04-25 17:13:51 +0200

    MyTabBar

---
+++ lib/MyTabBar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class MyTabBar extends StatelessWidget {
+  const MyTabBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Yusto')),
+        ),
+        bottomNavigationBar: _menu(theme),
+        body: _tabBarView(),
+      ),
+    );
+  }
+}
+
+Widget _menu(ThemeData theme) {
+  return TabBar(
+    labelColor: theme.colorScheme.primary,
+    unselectedLabelColor: theme.colorScheme.onSurface,
+    indicatorColor: theme.colorScheme.primary,
+    tabs: const [
+      Tab(
+        icon: Icon(Icons.settings),
+        text: "Einstellungen",
+      ),
+      Tab(
+        icon: Icon(Icons.list),
+        text: "Gewohnheiten",
+      ),
+      Tab(
+        icon: Icon(Icons.show_chart),
+        text: "Statistik",
+      ),
+    ],
+  );
+}
+
+Widget _tabBarView() {
+  return const TabBarView(
+    children: [
+      Center(child: Text("Einstellungen")),
+      Center(child: Text("Gewohnheiten")),
+      Center(child: Text("Statistik")),
+    ],
+  );
+}
