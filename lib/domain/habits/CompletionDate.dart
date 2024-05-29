defb13307f6b21bb71fbfd98d1c957117c5797e9
felixfischer <ff@mindhoox.de>
2024-05-29 09:59:55 +0200

    CompletionDate

--- lib/domain/habits/CompletionDate.dart
+++ lib/domain/habits/CompletionDate.dart
@@ -4,9 +4,21 @@ import 'package:objectbox/objectbox.dart';
 class CompletionDate {
   int id = 0;
 
+  @Transient()
+  late DateTime date;
+
   int dateMillis;
 
   CompletionDate({
     required this.dateMillis,
-  });
+  }) : date = DateTime.fromMillisecondsSinceEpoch(dateMillis, isUtc: true);
+
+  CompletionDate.withDate({
+    required this.date,
+  }) : dateMillis = date.toUtc().millisecondsSinceEpoch;
+
+  @override
+  String toString() {
+    return 'CompletionDate{id: $id, date: $date}';
+  }
 }
