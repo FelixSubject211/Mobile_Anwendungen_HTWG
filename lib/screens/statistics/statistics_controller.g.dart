ccbf66a6b398c172d9e2e87f72f9b3961891de89
FelixSubject211 <ff@mindhoox.de>
2024-06-10 14:36:15 +0200

    wip

---
+++ lib/screens/statistics/statistics_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'statistics_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$statisticDefaultControllerHash() =>
+    r'05ed3b93ece20d4e8b42b94f64d4deceb7bc2d5f';
+
+/// See also [StatisticDefaultController].
+@ProviderFor(StatisticDefaultController)
+final statisticDefaultControllerProvider = AutoDisposeNotifierProvider<
+    StatisticDefaultController, StatisticModel>.internal(
+  StatisticDefaultController.new,
+  name: r'statisticDefaultControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$statisticDefaultControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$StatisticDefaultController = AutoDisposeNotifier<StatisticModel>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
