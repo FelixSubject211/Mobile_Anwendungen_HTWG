3dc89696b10f58ab7c056b4543840f9cdf456a85
Muhammed Ergül <futurama.99@hotmail.de>
2024-07-24 14:31:02 +0200

    new unit test - domain habits - completed

--- test/unitTests/domain/habit/habit_default_repository_test.dart
+++ test/unitTests/domain/habit/habit_default_repository_test.dart
@@ -155,5 +155,87 @@ void main() {
 
       verify(mockDatabase.deleteHabit(any)).called(1);
     });
+
+    test('should reorder habits', () async {
+      // Arrange
+      final habit1 = Habit(
+        id: 1,
+        name: 'Test Habit 1',
+        index: 0,
+        creationDate: DateTime.now(),
+        completionDates: [],
+      );
+      final habit2 = Habit(
+        id: 2,
+        name: 'Test Habit 2',
+        index: 1,
+        creationDate: DateTime.now(),
+        completionDates: [],
+      );
+
+      final dbHabits = [
+        DatabaseHabit(
+          id: habit1.id,
+          name: habit1.name,
+          index: habit1.index,
+          creationDate: habit1.creationDate.millisecondsSinceEpoch
+        ),
+        DatabaseHabit(
+          id: habit2.id,
+          name: habit2.name,
+          index: habit2.index,
+          creationDate: habit2.creationDate.millisecondsSinceEpoch
+        ),
+      ];
+
+      when(mockDatabase.listHabits()).thenReturn(dbHabits);
+
+      // Act
+      repository.reorderHabit(0, 1);
+
+      // Assert
+      verify(mockDatabase.reorderHabit(0, 1)).called(1);
+    });
+
+    test('should return next index', () async {
+      // Arrange
+      when(mockDatabase.nextIndex()).thenReturn(1);
+
+      // Act
+      final index = repository.nextIndex();
+
+      // Assert
+      expect(index, 1);
+      verify(mockDatabase.nextIndex()).called(1);
+    });
+
+    test('should get habit by id', () async {
+      // Arrange
+      final habit = Habit(
+        id: 1,
+        name: 'Test Habit',
+        index: 0,
+        creationDate: DateTime.now(),
+        completionDates: [],
+      );
+
+      final dbHabit = DatabaseHabit(
+        id: habit.id,
+        name: habit.name,
+        index: habit.index,
+        creationDate: habit.creationDate.millisecondsSinceEpoch
+      );
+
+      when(mockDatabase.getById(habit.id)).thenReturn(dbHabit);
+
+      // Act
+      final fetchedHabit = repository.getById(habit.id);
+
+      // Assert
+      expect(fetchedHabit, isNotNull);
+      expect(habitEqualsIgnoringCreationDate(fetchedHabit!, habit), isTrue);
+      verify(mockDatabase.getById(habit.id)).called(1);
+    });
+
   });
 }
