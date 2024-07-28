9e95e13fcae94cc5b77746951f67d4c2e6aab25a
Melanie Galip <me391gal@htwg-konstanz.de>
2024-07-28 16:20:05 +0200

    fix: export database mock.

--- test/widgetTests/helper/mock_database.dart
+++ test/widgetTests/helper/mock_database.dart
@@ -1,6 +1,5 @@
 import 'package:mobile_anwendungen/database/database.dart';
 import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 
 @GenerateNiceMocks([MockSpec<Database>()])
-import 'mock_database.mocks.dart';
+export 'mock_database.mocks.dart';
